ef774d98ffe469b6c44f503fc3cc9ce760d08e84
ejacquez5 <ejacquez5@gmail.com>
2020-04-01 09:58:12 -0600

    updated name to Ui_VmSystemSettings

--- VmSystemSettings.py
+++ VmSystemSettings.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Form(object):
+class Ui_VmSystemSettings(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(500, 659)
@@ -229,7 +229,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_Form()
+    ui = Ui_VmSystemSettings()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
