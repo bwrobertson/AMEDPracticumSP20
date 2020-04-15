66192a6c3bb4330f7ee1230ce16f0f13ac97754d
Ben Robertson <35968223+bwrobertson@users.noreply.github.com>
2020-04-15 17:35:42 -0600

    added DB delete/import

--- ManageVulnerablePrograms.py
+++ ManageVulnerablePrograms.py
@@ -69,6 +69,58 @@ class Ui_ManageVulnerablePrograms(object):
         for key in self.VulnerableProgramsList:
             self.VulnerableProgramsList[key]['File'] = base64.decodebytes(self.VulnerableProgramsList[key]['File'])
 
+    def importVP(self):
+        try:
+            client = MongoClient(Ui_DBConfiguration.dbConnection)
+        except:
+            client = MongoClient("mongodb+srv://BWR:benji@adventurermart-j760a.mongodb.net/test")
+        db = client.Test
+        data = db["VulnerablePrograms"]
+        file = self.destinationDirectoryLINEEDIT.text()
+        fileName = os.path.split(file)
+        fileName = fileName[1]#.replace('.',';')
+        scenario = {'name' : fileName}
+        with open(file, 'rb') as eFile:
+            encoded_img = base64.b64encode(eFile.read())
+        scenario['File'] = encoded_img
+
+        print("beginning push of " + fileName)
+        data.insert_one(scenario)
+        self.databseTREEWIDGET.clear()
+        self.setupTree(self)
+        self.retranslateUi(self)
+        QMessageBox.about(self, "Success", fileName + " successfully uploaded!")
+
+    def deleteVP(self):
+        _translate = QtCore.QCoreApplication.translate
+        __sortingEnabled = self.databseTREEWIDGET.isSortingEnabled()
+        self.databseTREEWIDGET.setSortingEnabled(False)
+        try:
+            client = MongoClient(Ui_DBConfiguration.dbConnection)
+        except:
+            client = MongoClient("mongodb+srv://BWR:benji@adventurermart-j760a.mongodb.net/test")
+        db = client.Test
+        data = db["VulnerablePrograms"]
+        self.count = 0
+        deletion = ""
+        x=0
+        y=0
+        id=""
+        for collection in data.find():
+            for key in collection:
+                if(key=="_id"):
+                    id = collection[key]
+                if(key!="_id" and key!="name"):
+                    if(self.tree["parent{0}".format(x)].checkState(0)==QtCore.Qt.Checked):
+                        data.delete_one({'_id': id})
+                        print(key + " deleted from database.")
+            x+=1
+        self.databseTREEWIDGET.clear()
+        self.setupTree(self)
+        self.retranslateUi(self)
+        QMessageBox.about(self, "Success", "Items Deleted Succesfully.")
+
+
     def setupUi(self, ManageVulnerablePrograms):
 
 
@@ -135,9 +187,16 @@ class Ui_ManageVulnerablePrograms(object):
         self.importButton = QtWidgets.QPushButton(self.layoutWidget)
         self.importButton.setObjectName("importButton")
         self.importButton.setEnabled(False)
+        self.importButton.clicked.connect(self.importVP)
         self.horizontalLayout_2.addWidget(self.importButton)
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem1)
+        self.deleteButton = QtWidgets.QPushButton(self.layoutWidget)
+        self.deleteButton.setObjectName("deleteButton")
+        self.deleteButton.clicked.connect(self.deleteVP)
+        self.horizontalLayout_2.addWidget(self.deleteButton)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem2)
         self.nextButton = QtWidgets.QPushButton(self.layoutWidget)
         self.nextButton.setObjectName("nextButton")
         self.nextButton.clicked.connect(self.selectVulnerablePrograms)
@@ -154,7 +213,7 @@ class Ui_ManageVulnerablePrograms(object):
         i = 0
         client = MongoClient("mongodb+srv://BWR:benji@adventurermart-j760a.mongodb.net/test")
         db = client.Test
-        data = db["VulnerableProgramss"]
+        data = db["VulnerablePrograms"]
         _translate = QtCore.QCoreApplication.translate
         ManageVulnerablePrograms.setWindowTitle(_translate("ManageVulnerablePrograms", "Manage Vulnerable Programs"))
         self.destinationDirectoryLABEL.setText(_translate("ManageVulnerablePrograms", "Destination Directory:"))
@@ -171,6 +230,7 @@ class Ui_ManageVulnerablePrograms(object):
             i+=1
         self.databseTREEWIDGET.setSortingEnabled(__sortingEnabled)
         self.backBUTTON.setText(_translate("ManageVulnerablePrograms", "Back"))
+        self.deleteButton.setText(_translate("ManageExploits", "Delete"))
         self.importButton.setText(_translate("ManageVulnerablePrograms", "Import"))
         self.nextButton.setText(_translate("ManageVulnerablePrograms", "Next"))
 
@@ -182,7 +242,7 @@ class Ui_ManageVulnerablePrograms(object):
         except:
             client = MongoClient("mongodb+srv://BWR:benji@adventurermart-j760a.mongodb.net/test")
         db = client.Test
-        data = db["VulnerableProgramss"]
+        data = db["VulnerablePrograms"]
         for collection in data.find():
             self.tree["parent{0}".format(x)] = QtWidgets.QTreeWidgetItem(self.databseTREEWIDGET)
             self.tree["parent{0}".format(x)].setCheckState(0, QtCore.Qt.Unchecked)
