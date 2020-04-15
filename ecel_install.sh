c2935103815fd09e66e6a4b481accb0e5d2b7e27
Stefany Carrillo <scarrillo7@miners.utep.edu>
2020-04-15 14:41:21 -0600

    adding ecel installation script

---
+++ ecel_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Needs to have chmod 755 ecel_install
+# remember to sudo su
+
+kill -9 $(ps aux | grep -i apt | grep usr | cut -d" " -f7) # removes the lock on update
+apt-get update
+apt-get install -y gcc make curl python-pip python3-pip git expect
+pip3 install pymongo dnspython
+pip install pymongo dnspython
+apt-get install python3-pyqt5
+sleep 3
+git clone https://github.com/kevinHonsaker01/ecel.git
+cd ecel
+echo ECEL_HOME="\"${PWD}\"" >> /etc/environment
+source /etc/environment
+
+chmod 755 collectors
+chmod 755 script.exp
+./script.exp 
+
+# to run collectors ./collectors (inside the ecel folder; exports data to Ben's DB)
