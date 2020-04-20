17c8a8fb0c1e0fe322cde9bb81704a7b9dee6319
ejacquez5 <ejacquez5@gmail.com>
2020-04-20 11:32:24 -0600

    Update SuggestedSetup.py

--- SuggestedSetup.py
+++ SuggestedSetup.py
@@ -140,7 +140,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Suggested Setup", "Suggested Setup"))
         self.addVmBUTTON.setText(_translate("Form", "Add Victim || PoV"))
         self.label_2.setText(_translate("Form", "Victim"))
         self.nextBUTTON.setText(_translate("Form", "Next"))
